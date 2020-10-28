98cd1449797e87038f7aa540fb8f5083cec62512
Manuel Fonseca <manuel.fonseca45499@alumnos.udg.mx>
2020-10-27 19:52:26 -0600

    administrador de particulas

---
+++ adm_part.py
@@ -0,0 +1,33 @@
+from .particula import Particula
+
+class Adm_part:
+    def __init__(self):
+        self .__particula = []
+
+    def agregar_final(self, particula:Particula):
+        self.__particula.append(particula)
+
+    def agregar_inicio(self, particula:Particula):
+        self.__particula.insert(0,particula)
+
+    def mostrar(self):
+        for particula in self.__particula:
+            print(particula)
+
+    def __str__(self):
+        return "".join(
+            str(particula) + '\n' for particula in self.__particula 
+        )      
+
+
+
+# l01 = Particula(id=100,origen_x=56,origen_y=34,destino_x=345,destino_y=400,
+#                 velocidad=34,red=23,green=123,blue=200,distancia=0)
+# l02 = Particula(id=200,origen_x= 100,origen_y= 140,destino_x= 400,destino_y= 440,
+#                 velocidad= 120,red= 300,green= 12,blue=0,distancia= 0)
+
+# adm_part = Adm_part()
+# adm_part.agregar_final(l01)
+# adm_part.agregar_inicio(l02)
+# adm_part.agregar_inicio(l01)
+#  adm_part.mostrar()
